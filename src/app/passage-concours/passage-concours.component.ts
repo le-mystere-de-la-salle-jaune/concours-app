b82a1e242f2f9b769d7ec9de46dbed058fbabed6
Redor <matthieu.redor@hotmail.fr>
2018-07-30 17:31:56 +0200

    Amémioration dede la page passage concours

--- src/app/passage-concours/passage-concours.component.ts
+++ src/app/passage-concours/passage-concours.component.ts
@@ -16,25 +16,26 @@ export class PassageConcoursComponent implements OnInit {
    * id:number
    * Id du stagiaire en cours
    */
-  id:number
+  id:number = 0
   /**
    * idConcours:number
    * Id du concours en cours
    */
-  idConcours:number
+  idConcours:number = null
   /**
    * stagiaires:Stagiaire[]
    * Liste des stagiaires enregistrer dans l'application
    * Note : À retirer à l'avenir en utilisant une méthode de type getById()
    */
-  stagiaires:Stagiaire[] = []
-  concours:Concours[] = []
-  questions:Question[] = []
-  question:Question = new Question(0,"",[])
+  stagiaires:Stagiaire[] = [new Stagiaire(0,"","","","")]
+  concours:Concours[] = [new Concours(0,"",0,0)]
+  questions:Question[] = [new Question(0,"",[new Option(0,"",false)])]
+  question:Question = new Question(0,"",[new Option(0,"",false)])
   questionEnCours = 0
-  nombreQuestions = -1;
+  nombreQuestions = 0
+  idReponse:number;
 
-  constructor(private route: ActivatedRoute, private stagiaireService:StagiaireService, private concoursService:ConcoursService, private questionService:QuestionService, router:Router) { 
+  constructor(private route: ActivatedRoute, private stagiaireService:StagiaireService, private concoursService:ConcoursService, private questionService:QuestionService, private router:Router) { 
     this.id = +route.snapshot.paramMap.get("id")
     this.idConcours = +route.snapshot.paramMap.get("idConcours")
 
@@ -42,13 +43,13 @@ export class PassageConcoursComponent implements OnInit {
     this.stagiaireService.listerStagiaires().subscribe(
       value => this.stagiaires = value,
       error => console.log("Error : ",error),
-      () => console.log("La récupératon de données s'est bien passé")
+      () => console.log("La récupératon des données s'est bien passé")
     )
     //this.concoursService.listerConcours(route.snapshot.paramMap.get("id")).then((liste:Concours[])=> { this.concours = liste }).catch((error:any)=> console.log(error) )
     this.concoursService.listerConcours(route.snapshot.paramMap.get("id")).subscribe(
       value => this.concours = value,
       error => console.log("Error : ", error),
-      () => console.log("La récupératon de données s'est bien passé")
+      () => console.log("La récupératon des données s'est bien passé")
     )
     //this.questionService.listerQuestions().then((liste:Question[])=> {this.questions = liste;this.question = this.questions[this.questionEnCours] }).catch((error:any)=> console.log(error))
     this.questionService.listerQuestions().subscribe(
@@ -57,12 +58,24 @@ export class PassageConcoursComponent implements OnInit {
         this.question = this.questions[this.questionEnCours]
       },
       error => console.log("Error : ",error),
-      () => console.log("La récupératon de données s'est bien passé")
+      () => console.log("La récupératon des données s'est bien passé")
     )
     this.nombreQuestions = this.questions.length
   }
 
   submit(monForm:any){
+
+    if(this.questionEnCours < this.nombreQuestions){
+      this.questionEnCours++
+      this.question = this.questions[this.questionEnCours]
+    }
+    else if(this.questionEnCours == this.nombreQuestions){
+      this.questionEnCours++
+      this.question = new Question(0,"Vous avez fini le questionnaire",[new Option(0,"COMPRIS",true)])
+    }
+    else{
+      this.router.navigateByUrl('choix-stagiaires')
+    }
 
   }
 
