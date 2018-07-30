e69c769cc13dc35e1357a420be56f3065ee90119
Redor <matthieu.redor@hotmail.fr>
2018-07-30 15:30:05 +0200

    Amémioration de la page  éditer stagiaire

--- src/app/editer-stagiaire/editer-stagiaire.component.ts
+++ src/app/editer-stagiaire/editer-stagiaire.component.ts
@@ -14,13 +14,25 @@ export class EditerStagiaireComponent implements OnInit {
   id:string
   // la propriété Stagiaire est mise à jour automatiquement avec la saisie utilisateur
   // grâce au binding bi-directionnel  [(ngModel)]
-  monStagiaire:Stagiaire
+  monStagiaire:Stagiaire = new Stagiaire(0,"","","","")
 
   constructor(private route: ActivatedRoute,private service:StagiaireService, private router:Router) {
     this.id = this.route.snapshot.paramMap.get("id")
-    this.monStagiaire = new Stagiaire(+this.id,"","","","")
+
+    let stagiaires:Stagiaire[]
+    service.listerStagiaires().subscribe(
+      value => {
+        stagiaires = value;
+        this.monStagiaire = stagiaires[+this.id - 1]
+      },
+      error => console.log("Error : ",error),
+      () => console.log("La récupération de données s'est bien passé")
+    )
+
+    
     //service.getStagiaireById(+this.id).then( (stagiaire:Stagiaire) => this.monStagiaire=stagiaire )
-   }
+   
+  }
 
   submit(monForm:any){
     
